bf80bd87da78701ece1b5753af771b4fb28b490c
S3RG1U <80359189+S3RG1U@users.noreply.github.com>
2023-03-31 21:27:06 +0300

    test routes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'auth'], function(){
+//Route::group(['middleware' => 'auth'], function(){
     Route::get('/posts', [PostController::class, 'index']);
     Route::get('/posts/{id}', [PostController::class, 'show'])->name('show');
     Route::post('/posts/store', [PostController::class, 'store'])->name('store');
     Route::get('/posts/delete/{id}', [PostController::class, 'destroy'])->name('destroy');
     Route::get('/posts', [PostController::class, 'search'])->name('posts.search');
-});
-Route::get('/', function () {
-    return redirect('/login');
-});
-
-Auth::routes();
+//});
+//Route::get('/', function () {
+//    return redirect('/login');
+//});
+//
+//Auth::routes();
 
